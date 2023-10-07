c8372d7ba1cef1a20c8a860fa462162585db0d8f
mjsimo99 <mohashark21@gmail.com>
2023-10-07 19:14:23 +0100

    update

--- src/implementation/AgenceImpl.java
+++ src/implementation/AgenceImpl.java
@@ -3,6 +3,7 @@ package implementation;
 import dto.Agence;
 import dto.Compte;
 import dto.Employe;
+import helper.DatabaseConnection;
 import interfeces.IAgence;
 
 import java.sql.Connection;
@@ -12,21 +13,21 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class AgenceImpl implements IAgence {
-    private final Connection connection;
-
-    public AgenceImpl(Connection connection) {
-        this.connection = connection;
-    }
 
     private static final String ADDAGENCE = "INSERT INTO Agences (code, nom, adresse, tel) VALUES (?, ?, ?, ?)";
     private static final String SEARCHBYCODE = "SELECT * FROM Agences WHERE code = ?";
+    private static final String DELETEBYCODE = "DELETE FROM Agences WHERE code = ?";
+    private static final String SEARCHBYADRESS = "SELECT * FROM Agences WHERE adresse = ?";
+    private static final String UPDATEBYCODE =  "UPDATE Agences SET nom = ?, adresse = ?, tel = ? WHERE code = ?";
+
+
 
 
     @Override
     public Optional<Agence> Add(Agence agence) {
+        Connection connection = DatabaseConnection.getConn();
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(ADDAGENCE)) {
             preparedStatement.setString(1, agence.getCode());
@@ -43,15 +44,20 @@ public class AgenceImpl implements IAgence {
         }
         return Optional.empty();
 
+
     }
 
     @Override
-    public Optional<Agence> SearchByCode(Agence agence) {
+    public Optional<Agence> SearchByCode(String code) {
+        Connection connection = DatabaseConnection.getConn();
+
         try (PreparedStatement preparedStatement = connection.prepareStatement(SEARCHBYCODE)) {
-            preparedStatement.setString(1, agence.getCode());
+            preparedStatement.setString(1, code);
 
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
+                Agence agence = new Agence();
+                agence.setCode(resultSet.getString("code"));
                 agence.setNom(resultSet.getString("nom"));
                 agence.setAdresse(resultSet.getString("adresse"));
                 agence.setTel(resultSet.getString("tel"));
@@ -65,16 +71,38 @@ public class AgenceImpl implements IAgence {
     }
 
 
-
     @Override
     public boolean Delete(String code) {
-        return false;
+        Connection connection = DatabaseConnection.getConn();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(DELETEBYCODE)) {
+            preparedStatement.setString(1, code);
+
+            int rowsDeleted = preparedStatement.executeUpdate();
+            return rowsDeleted > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Optional<Agence> Update(Agence agence) {
-        return Optional.empty();
+        Connection connection = DatabaseConnection.getConn();
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(UPDATEBYCODE)
+        ) {
+            preparedStatement.setString(1, agence.getNom());
+            preparedStatement.setString(2, agence.getAdresse());
+            preparedStatement.setString(3, agence.getTel());
+            preparedStatement.setString(4, agence.getCode());
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            return (rowsUpdated > 0) ? Optional.of(agence) : Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
     @Override
     public List<Agence> SearchByEmployee(Employe employe) {
@@ -82,8 +110,28 @@ public class AgenceImpl implements IAgence {
     }
 
     @Override
-    public List<Compte> SearchByAdress(String adresse) {
-        return null;
+    public List<Agence> SearchByAdress(String adresse) {
+        List<Agence> agences = new ArrayList<>();
+        Connection connection = DatabaseConnection.getConn();
+
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SEARCHBYADRESS)) {
+            preparedStatement.setString(1, adresse);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String agenceCode = resultSet.getString("code");
+                String nom = resultSet.getString("nom");
+                String tel = resultSet.getString("tel");
+
+                Agence agence = new Agence(agenceCode, nom, adresse, tel, null, null, null);
+                agences.add(agence);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return agences;
     }
 
 }
